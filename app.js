b94ccd8ee9bb3dfff81203a4cec50b0141533347
feheed <fmjuwaihel@gmail.com>
2022-03-06 10:30:28 +0300

    middleware

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-//                    importation
-
+//                 importation
 const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
+const cors = require("cors"); // to connect with frontend
+const path = require("path");
 // const passport = require("passport");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 // const logger = require("./middleware/logger");
 // const errorHandler = require("./middleware/errorHandler");
 //                   DB
 const connectDB = require("./db/database");
-
+//                 Passport
+// const { localStrategy, jwtStrategy } = require("./middleware/passport");
+//                 express
 const app = express();
 app.use(express.json());
+//                 Middleware
+app.use(cors());
+app.use(morgan("dev"));
 
-//handling error
+// Passport
+// app.use(passport.initialize());
+// passport.use(localStrategy);
+// passport.use(jwtStrategy);
+
+//              handling error
 app.use((req, res, next) => {
   res.status(404).json({ message: "Path not found" });
 });
+// Routes
+app.use("/media", express.static(path.join(__dirname, "media")));
 
 connectDB(); //connect to the database
 const PORT = 8000; //connection to a host
