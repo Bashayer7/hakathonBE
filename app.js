784008c3004e83d7505f3743ac43a1a46a21a962
feheed <fmjuwaihel@gmail.com>
2022-03-08 08:41:32 +0300

    fixing issue

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const morgan = require("morgan");
 const logger = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 //               Routes
-app.use("/media", express.static(path.join(__dirname, "media")));
+
 const userRoutes = require("./apis/users/users.routes");
-app.use("/api", userRoutes);
+
 //                   DB
 const connectDB = require("./db/database");
 //                 Passport
 const { localStrategy, jwtStrategy } = require("./middleware/passport");
-express;
+
 const app = express();
 app.use(express.json());
 //                 Middleware
@@ -36,7 +36,9 @@ passport.use(jwtStrategy);
 app.use((req, res, next) => {
   res.status(404).json({ message: "Path not found" });
 });
-
+//Routes
+app.use("/api", userRoutes);
+app.use("/media", express.static(path.join(__dirname, "media")));
 app.use(errorHandler);
 connectDB(); //connect to the database
 const PORT = 8000; //connection to a host
